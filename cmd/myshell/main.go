4b6c78fd592aa21718a384e737642995c56f9756
Doni Rubiagatra <doni@zero-one-group.com>
2024-12-30 11:01:09 +0700

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,13 +7,31 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	defaultCommands := []string{"ls"}
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input: ", err)
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+			os.Exit(1)
+		}
+
+		strippedCommand := command[:len(command)-1]
+		if strippedCommand == "exit" {
+			fmt.Println("Exiting")
+			os.Exit(0)
+		}
+
+		for _, v := range defaultCommands {
+			if v == strippedCommand {
+				fmt.Println("Do the Command")
+				continue
+			}
+		}
+
+		fmt.Println(strippedCommand + ": command not found")
 	}
-	fmt.Println(input[:len(input)-1] + ": command not found")
+
 }
