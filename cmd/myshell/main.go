eebf7f2a6f4d2a1ef9daa41d45ef5d35896b8617
Doni Rubiagatra <doni@zero-one-group.com>
2024-12-30 10:25:51 +0700

    first task of codecrafter shell

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,8 +10,6 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
